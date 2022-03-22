3369e8f1791ba1acb9379dd7e8cdc0a107c47bc6
Najeeb <najeeb.rehman@digitify.com>
2022-03-22 14:27:43 +0500

    Updated AnalyticsTracker protocol to send more info in some methods

--- YAPCore/Classes/AnalyticsTracker.swift
+++ YAPCore/Classes/AnalyticsTracker.swift
@@ -8,7 +8,7 @@ import Foundation
 
 public protocol AnalyticsTracker {
     func trackFirebaseEvent(_ eventName: String, withParameters eventParameters: [String: Any])
-    func trackAdjustEventWithToken(_ eventToken: String)
-    func trackLeanplumEvent(_ event: String)
+    func trackAdjustEventWithToken(_ eventToken: String, customerId: String?, andParameters parameters: [String: Any]?)
+    func trackLeanplumEvent(_ event: String, withParameters parameters: [String: Any])
     func setLeanplumUserAttributes(_ attributes: [String: Any])
 }
